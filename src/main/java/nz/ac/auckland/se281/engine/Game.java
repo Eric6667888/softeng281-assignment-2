ba3ad4eaf71fd2e8cd2fecc56bb81b279a1c6ace
Eric Liu <lzhe729@aucklanduni.ac.nz>
2025-05-21 11:52:02 +1200

    print welcome message and record player's information

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -5,12 +5,18 @@ import nz.ac.auckland.se281.cli.MessageCli;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
+  private static int numRounds;
+  private int currentRound;
+  private String playerName;
 
   public Game() {}
 
   public void newGame(Difficulty difficulty, int numRounds, String[] options) {
-    String namePlayer = options[0];
-    MessageCli.WELCOME_PLAYER.printMessage(namePlayer);
+    this.playerName = options[0];
+    MessageCli.WELCOME_PLAYER.printMessage(playerName);
+    this.numRounds = numRounds;
+    this.currentRound = 1;
+    MessageCli.WELCOME_PLAYER.printMessage(playerName);
   }
 
   public void play() {}
