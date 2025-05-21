2ca733119982a75632e94b0a9507ddd060998532
Eric Liu <lzhe729@aucklanduni.ac.nz>
2025-05-21 12:33:00 +1200

    Read player's colour inputs and check it is valid or not

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -2,24 +2,52 @@ package nz.ac.auckland.se281.engine;
 
 import nz.ac.auckland.se281.Main.Difficulty;
 import nz.ac.auckland.se281.cli.MessageCli;
+import nz.ac.auckland.se281.cli.Utils;
+import nz.ac.auckland.se281.model.Colour;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
-  private static int numRounds;
+  private int numRounds;
   private int currentRound;
   private String playerName;
+  public Difficulty difficultyLevel;
+  private int playerPoints;
+  private int aiPoints;
 
   public Game() {}
 
   public void newGame(Difficulty difficulty, int numRounds, String[] options) {
+
     this.playerName = options[0];
     MessageCli.WELCOME_PLAYER.printMessage(playerName);
     this.numRounds = numRounds;
     this.currentRound = 1;
-    MessageCli.WELCOME_PLAYER.printMessage(playerName);
+    this.difficultyLevel = difficulty;
   }
 
-  public void play() {}
+  public void play() {
+    if (currentRound > numRounds) {
+      MessageCli.PRINT_END_GAME.printMessage();
+      return;
+    }
+    MessageCli.START_ROUND.printMessage(currentRound, numRounds);
+    currentRound++;
+    MessageCli.ASK_HUMAN_INPUT.printMessage();
+    // Get the input from the user
+    String input = Utils.scanner.nextLine();
+    String[] inputs = input.split(" ");
+    if (inputs.length != 2) {
+      MessageCli.INVALID_HUMAN_INPUT.printMessage();
+      return;
+    }
+    if (Colour.fromInput(inputs[0]) == null || Colour.fromInput(inputs[1]) == null) {
+      MessageCli.INVALID_HUMAN_INPUT.printMessage();
+      return;
+    } else {
+      Colour playerColour = Colour.fromInput(inputs[0]);
+      Colour playerGuess = Colour.fromInput(inputs[1]);
+    }
+  }
 
   public void showStats() {}
 }
